a26f9d2256824cb088ff3e4ed8e7aaacb80137f0
jimpatton <jimpatton5150@gmail.com>
2025-02-22 20:09:47 -0500

    init commit

---
+++ Models/Actor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BmdbNetWeb.Models;
+
+[Table("Actor")]
+[Index("FirstName", "LastName", "BirthDate", Name = "UC_Actor", IsUnique = true)]
+public partial class Actor
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string FirstName { get; set; } = null!;
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string LastName { get; set; } = null!;
+
+    [StringLength(1)]
+    [Unicode(false)]
+    public string Gender { get; set; } = null!;
+
+    public DateOnly BirthDate { get; set; }
+
+    [InverseProperty("Actor")]
+    public virtual ICollection<Credit> Credits { get; set; } = new List<Credit>();
+}
---
+++ Models/MovieCopy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BmdbNetWeb.Models;
+
+[Keyless]
+[Table("MovieCopy")]
+public partial class MovieCopy
+{
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [StringLength(75)]
+    [Unicode(false)]
+    public string Title { get; set; } = null!;
+
+    public int Year { get; set; }
+
+    [StringLength(5)]
+    [Unicode(false)]
+    public string Rating { get; set; } = null!;
+
+    [StringLength(75)]
+    [Unicode(false)]
+    public string Director { get; set; } = null!;
+}
